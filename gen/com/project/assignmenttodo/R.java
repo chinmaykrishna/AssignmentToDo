b36ff18f79fb7a03d079a6d5a69a8c3124ac2ee9
Pranav Singh <singh.pranavkumar10@gmail.com>
2014-05-12 22:16:23 +0530

    changed buttons of subtask menu

--- gen/com/project/assignmenttodo/R.java
+++ gen/com/project/assignmenttodo/R.java
@@ -28,15 +28,15 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080016;
-        public static final int assignment_add=0x7f080006;
+        public static final int assignment_add=0x7f080005;
         public static final int assignment_add_button=0x7f08000a;
         public static final int assignment_name_row=0x7f08000d;
         public static final int assignment_remove_button=0x7f08000b;
         public static final int assignment_text=0x7f080004;
-        public static final int cancel_assignment_add=0x7f080005;
-        public static final int cancel_course_add=0x7f080009;
+        public static final int cancel_assignment_add=0x7f080006;
+        public static final int cancel_course_add=0x7f080008;
         public static final int checkBox1=0x7f080013;
-        public static final int course_add=0x7f080008;
+        public static final int course_add=0x7f080009;
         public static final int course_add_button=0x7f080001;
         public static final int course_name_row=0x7f08000e;
         public static final int course_remove_button=0x7f080000;
@@ -45,10 +45,10 @@ public final class R {
         public static final int refresh_assignment_button=0x7f08000c;
         public static final int refresh_course_button=0x7f080002;
         public static final int refresh_subtask_button=0x7f080012;
-        public static final int subtask_add_button=0x7f08000f;
+        public static final int subtask_add_button=0x7f080010;
         public static final int subtask_desc=0x7f080014;
-        public static final int subtask_remove_button=0x7f080010;
-        public static final int task_list=0x7f080011;
+        public static final int subtask_remove_button=0x7f080011;
+        public static final int task_list=0x7f08000f;
         public static final int textView1=0x7f080003;
     }
     public static final class layout {
